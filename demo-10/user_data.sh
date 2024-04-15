73215be8aaf160945d6d534cc25da1076a0f64e3
Rahul Vishwakarma <rahulvishwakarma1181@gmail.com>
2024-04-15 13:30:06 +0000

    update

---
+++ demo-10/user_data.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+ls
+cat instance.tf
+cat cloudinit.tf
+cat  scripts/init.cfg
+cat  scripts/volumes.sh
+
+ssh-keygen -f mykey
+terraform apply
+
+vim terraform.tfstate
+ssh -i mykey ubuntu@52.210.97.92
+
+ls /dev/data/volume1
+df -h
+cat /var/log/cloud-init
+
+pvdisplay
+lvdisplay
---
+++ demo-9/ebs_demo.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+ls
+cat instance.tf
+terraform apply
+ssh-keygen -f mykey
+terraform apply
+
+vim terraform.tfstate
+ssh -i mykey -l ubuntu 52.18.152.235
+
+sudo -s
+df -h
+mkfs.ext4 /dev/xvdh
+mkdir /data
+mount /dev/xvdh /data
+df -h
+
+vim /etc/fstab
+man fstab
+unmount /data
+mount /data
+df -h
+
+cat /etc/fstab
+
+
+
